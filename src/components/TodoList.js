b8c4c4cd9a03b874b57b32ae18bf82d12d253d2c
grvsriharsha <grvsriharsha@gmail.com>
2020-11-12 22:12:52 +0530

    working now

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,20 +2,16 @@ import React, { useContext } from "react";
 import TodosContext from "../context";
 
 export default function TodoList() {
-  const { state, count, setCount } = useContext(TodosContext);
-  console.log("****************");
-  console.log(state.todos);
-  console.log("****************");
-
-  var todos = state.todos;
+  const { state, dispatch } = useContext(TodosContext);
   return (
     <div>
-      count is {count}
       <ul>
         {todos.map((todo) => {
-          <li key={todo.id}>
-            <span>{todo.text}</span>
-          </li>;
+          return (
+            <li key={todo.id}>
+              <span>{todo.text}</span>
+            </li>
+          );
         })}
       </ul>
     </div>
